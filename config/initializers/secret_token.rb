79288fa186a7ade1145c8a69a33cac0edecab2d3
Bader <baderjulien@hotmail.fr>
2013-12-03 14:52:37 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JournalDeBord::Application.config.secret_token = '431253aa337216dfb4b8c4bcccd11b35210344c5e32c2e0c810a0a94fa9c96b19df3dfe592e11a2d4220222dc5164333b1868ed81ad25fe0960d5e5c08887729'
